6bd7538941e236528314cdb46c1aa72edf5a8486
burn-c <chopcsom@gmail.com>
2020-02-18 22:19:12 -0300

    Styles: Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/06/D22-2763-006/D22-2763-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
