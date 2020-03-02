596e3ece2329d9a77e080da29ca999580830e3ab
burn-c <chopcsom@gmail.com>
2020-03-01 21:47:15 -0300

    Feacture: Calculating Total

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,6 +1,21 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: 'ADD_TO_CART',
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
     product,
+  };
+}
+
+export function updateAmount(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT',
+    id,
+    amount,
   };
 }
